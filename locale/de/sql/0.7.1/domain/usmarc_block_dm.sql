12c21bb2970932d187e8a36ca92a87501bec3af7
Thies Mueller <github@td00.de>
2021-05-20 12:09:26 +0000

    Da ist noch einer durchgerutscht...

--- locale/de/sql/0.7.1/domain/usmarc_block_dm.sql
+++ locale/de/sql/0.7.1/domain/usmarc_block_dm.sql
@@ -2,10 +2,10 @@ insert into %prfx%usmarc_block_dm values (0,'Kontrollinformationen, Zahlen und C
 insert into %prfx%usmarc_block_dm values (1,'Haupteintrag');
 insert into %prfx%usmarc_block_dm values (2,'Titel, Untertitel, Verlag, Reihe, Edition, Erscheinungsjahr');
 insert into %prfx%usmarc_block_dm values (3,'Physikalische Beschreibung, Seitenzahlen, Groesse');
-insert into %prfx%usmarc_block_dm values (4,'Informationen &uuml;ber die Serie');
+insert into %prfx%usmarc_block_dm values (4,'Informationen ueber die Serie');
 insert into %prfx%usmarc_block_dm values (5,'Inhaltsangabe, Zusammenfassung, Themenbereich und Anmerkungen');
 insert into %prfx%usmarc_block_dm values (6,'Schlagwoerter');
-insert into %prfx%usmarc_block_dm values (7,'Zusaetzliche Eintraege, nicht Serie-n und Themenbezogen');
+insert into %prfx%usmarc_block_dm values (7,'Zusaetzliche Eintraege, nicht Serien und Themenbezogen');
 insert into %prfx%usmarc_block_dm values (8,'Seriennebeneintragungen');
 insert into %prfx%usmarc_block_dm values (9,'Lokale Daten');
 
