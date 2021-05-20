deac0987bb5ae1ff678c31cdb1076e3e11ba9b88
Thies Mueller <github@td00.de>
2021-05-20 11:58:18 +0000

    like so?

--- locale/de/sql/0.7.1/domain/transaction_type_dm.sql
+++ locale/de/sql/0.7.1/domain/transaction_type_dm.sql
@@ -1,7 +1,3 @@
 insert into %prfx%transaction_type_dm values ('-p','Zahlung','Y');
 insert into %prfx%transaction_type_dm values ('-r','Gutschrift','N');
-<<<<<<< HEAD
 insert into %prfx%transaction_type_dm values ('+c','Gebuehr','N');
-=======
-insert into %prfx%transaction_type_dm values ('+c','Geb&uuml;hr','N');
->>>>>>> a17f6746050f61cded511848f6032083e35d267a
